1a68a0bc7d5d0da7e3013a84820d2c7b9f0a191e
Yuna-Aeryn <choiyoon011@outlook.kr>
2021-07-06 18:13:58 +0900

    unfinished for variable a loop

--- 19.py
+++ 19.py
@@ -1,12 +1,2 @@
-while True:
-    a = ""
-    a_temp = input()
-    for x in a_temp:
-        try:
-            if isinstance(int(x), int):
-                a.append(int(x))
-            elif isinstance(x, str):
-                a.append(ord(x) - 96)
-        except:
-            ('nothing')
-    print(a)
+elif x in capitalalphabet:
+a + str(capitalalphabet.index(x) + 1)
--- Dads.py
+++ Dads.py
@@ -6,15 +6,27 @@ import pyperclip
 root = Tk()
 root.title("Password Generator")
 
+alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+capitalalphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+
 # calculate
 def Calculate():
+
     # input > .com형태 > www 지우고 > ip로 바꾸고 > .지우고 > 곱하기 > 12자리까지 자르고 > 암호화
     b = urlparse(Userinput.get()).netloc
     if b[0:3] == "www":
         b = b[4:]
     ip = s.gethostbyname(b).replace('.', '')
-    # a는 나중에 정의함
-    password_temp = str(ip * a)[0:12]
+    global a
+    a = ""
+    for x in codeinput.get():
+        if x in alphabet:
+            a = a + str(alphabet.index(x) + 1)
+        elif x in capitalalphabet:
+            a = a + str(capitalalphabet.index(x) + 1)
+        else:
+            a = a + x
+    password_temp = str(ip * int(a))[0:12]
     global password_coded
     password_coded = str(chr(ord('A')+int(password_temp[0]))) + str(chr(ord('a')+int(password_temp[1]))) + str(chr(ord('a')+int(password_temp[2]))) + str(chr(ord('a')+int(password_temp[3]))) + str(password_temp[4:11]) + str(chr(33+int(password_temp[11])))
     print(password_coded)
@@ -25,12 +37,12 @@ def Calculate():
 def copy():
     pyperclip.copy(password_coded)
 
-# the input for a
-codeinput = Entry(root, width=50, borderwidth=5)
-codeinput.pack()
 # URL input
 Userinput = Entry(root, width=50, borderwidth=5)
 Userinput.pack()
+# the input for a
+codeinput = Entry(root, width=50, borderwidth=5)
+codeinput.pack()
 Userinput.xview_moveto(0)
 # for user to view url in case they can't
 Userview = Label(root)
@@ -43,17 +55,6 @@ Button1 = Button(root, text='Calculate', padx=50, pady=20, command=Calculate)
 Button1.pack()
 Button2 = Button(root, text='Copy', padx = 10, pady = 10, command = copy)
 Button2.pack()
-
-a = ""
-a_temp = codeinput.get()
-for x in a_temp:
-    try:
-        if isinstance(float(x), int):
-            a.append(float(x))
-        elif isinstance(str(x), str):
-            a.append(ord(x) - 96)
-    except:
-        print('nothing')
 
 root.mainloop()
 
