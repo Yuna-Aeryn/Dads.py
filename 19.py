9309cb5fe8208f4fab80d5ec3b29419db454ab0e
Yuna-Aeryn <choiyoon011@outlook.kr>
2021-07-08 12:36:50 +0900

    implemented 8 letter minimum limit, needs comments

--- 19.py
+++ 19.py
@@ -1,48 +1,9 @@
-from tkinter import *
-from urllib.parse import urlparse
-import socket as s
-import pyperclip
+while True:
+    actualUserinput_temp = Userinput.get()
+    if len(actualUserinput_temp) >= 8:
+        actualUserinput = actualUserinput_temp
+        break
+    elif len(actualUserinput_temp) < 8:
+        continue
 
-root = Tk()
-root.title("Password Generator")
-
-alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-capitalalphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-
-# calculate
-def Calculate():
-    # input > .com형태 > www 지우고 > ip로 바꾸고 > .지우고 > a 값 받고 > 곱하기 > 12자리까지 자르고 > 암호화
-    b = urlparse(Userinput.get()).netloc
-    if b[0:3] == "www":
-        b = b[4:]
-    ip = s.gethostbyname(b).replace('.', '')
-    password_temp = str(ip * 381591015)[0:12]
-    global password_coded
-    password_coded = "".join([str(chr(ord('A')+int(password_temp[0]))),str(chr(ord('a')+int(password_temp[1]))),str(chr(ord('a')+int(password_temp[2]))),str(chr(ord('a')+int(password_temp[3]))),str(password_temp[4:11]),str(chr(33+int(password_temp[11])))])
-    Conclusion.config(text=password_coded)
-    Userview.config(text=b)
-
-# copy
-def copy():
-    pyperclip.copy(password_coded)
-
-# URL input
-Userinput = Entry(root, width=50, borderwidth=5)
-Userinput.pack()
-Userinput.xview_moveto(0)
-# the input for a
-codeinput = Entry(root, width=50, borderwidth=5)
-codeinput.pack()
-# for user to view url in case they can't
-Userview = Label(root)
-Userview.pack()
-# actual password output
-Conclusion = Label(root)
-Conclusion.pack()
-# Button placements
-Button1 = Button(root, text='Calculate', padx=50, pady=20, command=Calculate)
-Button1.pack()
-Button2 = Button(root, text='Copy', padx = 10, pady = 10, command = copy)
-Button2.pack()
-
-root.mainloop()
+print(actualUserinput)
--- Dads.py
+++ Dads.py
@@ -10,32 +10,42 @@ root.title("Password Generator")
 alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 capitalalphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 
-
 def Calculate(event):
-    # input > .com형태 > www 지우고 > ip로 바꾸고 > .지우고 > a 값 받고 > np곱하기 > str으로 12자리까지 자르고 > 암호화
-    b = urlparse(Userinput.get()).netloc
-    if b[0:3] == "www":
-        b = b[4:]
-    ip = s.gethostbyname(b).replace('.', '')
-    a = ""
-    for x in codeinput.get():
-        if x in alphabet:
-            a = a + str(alphabet.index(x) + 1)
-        elif x in capitalalphabet:
-            a = a + str(capitalalphabet.index(x) + 1)
-        else:
-            a = a + x
-    password_temp = np.multiply(float(ip), float(a))
-    password_temp = ("%.17f" % float(password_temp)).rstrip('0').rstrip('.')
-    password_temp = str(password_temp)[0:12]
-    global password_coded
-    password_coded = "".join([str(chr(ord('A')+int(password_temp[0]))),str(chr(ord('a')+int(password_temp[1]))),str(chr(ord('a')+int(password_temp[2]))),str(chr(ord('a')+int(password_temp[3]))),str(password_temp[4:11]),str(chr(33+int(password_temp[11])))])
-    Conclusion.config(text=password_coded)
-    Userview.config(text=b)
+    actualcodeinput = codeinput.get()
+    if len(actualcodeinput) >= 8:
+        b = urlparse(Userinput.get()).netloc
+        if b[0:3] == "www":
+            b = b[4:]
+        ip = s.gethostbyname(b).replace('.', '')
+        a = ""
+        try:
+            for x in actualcodeinput:
+                if x in alphabet:
+                    a = a + str(alphabet.index(x) + 1)
+                elif x in capitalalphabet:
+                    a = a + str(capitalalphabet.index(x) + 1)
+                else:
+                    a = a + x
+            password_temp = np.multiply(float(ip), float(a))
+        except:
+            Conclusion.config(text='only characters or numbers')
+        password_temp = ("%.17f" % float(password_temp)).rstrip('0').rstrip('.')
+        password_temp = str(password_temp)[0:12]
+        global password_coded
+        password_coded = "".join(
+            [str(chr(ord('A') + int(password_temp[0]))), str(chr(ord('a') + int(password_temp[1]))),
+             str(chr(ord('a') + int(password_temp[2]))), str(chr(ord('a') + int(password_temp[3]))),
+             str(password_temp[4:11]), str(chr(33 + int(password_temp[11])))])
+        Userview.config(text=b)
+        Conclusion.config(text=password_coded)
+    else:
+        Userview.config(text="")
+        Conclusion.config(text='8 or more characters')
 
 # copy
 def Copy(event):
     pyperclip.copy(password_coded)
+
 
 # URL input
 Userinput = Entry(root, width=50, borderwidth=5)
@@ -50,6 +60,7 @@ Userview.pack()
 # actual password output
 Conclusion = Label(root)
 Conclusion.pack()
+
 
 #엔터키
 root.bind('<Return>',Calculate)
