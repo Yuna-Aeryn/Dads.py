6f5cc9ad1d36292dafd0cb029876777e63dc618e
Yuna-Aeryn <choiyoon011@outlook.kr>
2021-07-11 11:23:03 +0900

    comments, unicode change for a
    (cherry picked from commit 9e5a4231947c0052a5bf73fda5c5bc0025c466c0)

--- Dads.py
+++ Dads.py
@@ -41,10 +41,10 @@ def Calculate(event):
         Userview.config(text="")
         Conclusion.config(text='8 or more characters')
 
-
 # copy
 def Copy(event):
     pyperclip.copy(password_coded)
+
 
 
 
@@ -64,10 +64,10 @@ Conclusion = Label(root)
 Conclusion.grid(row=3, column=1)
 # label for showing
 urlhere = Label(root)
-urlhere.config(text='URL Here:')
+urlhere.config(text = 'URL Here:')
 urlhere.grid(row=0, column=0)
 passwordhere = Label(root)
-passwordhere.config(text='Pasword Here:')
+passwordhere.config(text = 'Pasword Here:')
 passwordhere.grid(row=1, column=0)
 
 
